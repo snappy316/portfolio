01435bb8928ca3e830a2e4a9467c716ed029a264
David Ker <snappy316@gmail.com>
2015-03-06 14:08:42 -0800

    Add test to check for theme

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -5,4 +5,9 @@ feature "Zurb Foundation" do
     visit root_path
     page.must_have_css('div.columns')
   end
+
+  scenario "Foundation is applying the correct theme" do
+    visit root_path
+    page.must_have_css('div.hero')
+  end
 end
