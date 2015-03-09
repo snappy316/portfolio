8105ecdf68c04df5c2a0092aacaa1385683a6b85
David Ker <snappy316@gmail.com>
2015-03-09 16:37:03 -0700

    Check for invalid data when creating a project

--- test/features/projects/creating_a_project_test.rb
+++ test/features/projects/creating_a_project_test.rb
@@ -2,17 +2,32 @@ require "test_helper"
 
 feature "Projects" do
   scenario "can add a new project" do
-    # Given a projects resource
-    visit projects_path
-    click_on("New Project")
-
-    # When I fill-in and submit a new project form
+    # Given the form is filled in with valid data
+    visit new_project_path
     fill_in("Name", with: "Test Project")
     fill_in("Technologies used", with: "Test Description")
+
+    # When I submit the form
     click_on("Create Project")
 
     # Then I should see the new project
     page.text.must_include("Project has been created")
     page.text.must_include("Test Project")
+  end
+
+  scenario "cannot add a new project" do
+    # Given the form is filled in with invalid data
+    visit new_project_path
+    fill_in("Name", with: "A")
+    fill_in("Technologies used", with: "")
+
+    # When I submit the form
+    click_on("Create Project")
+
+    # Then I should get an error message, and the form should be displayed again
+    current_path.must_match(/projects$/)
+    page.text.must_include("Project could not be saved")
+    page.text.must_include("Name is too short")
+    page.text.must_include("Technologies used can't be blank")
   end
 end
