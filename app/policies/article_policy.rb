f61b3c92b1f11694e6926127885b07c4483d1939
David Ker <snappy316@gmail.com>
2015-03-18 14:16:57 -0700

    Refactor article policy

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -16,4 +16,39 @@ class ArticlePolicy < ApplicationPolicy
   def destroy?
     editor?
   end
+
+  def publish?
+    editor?
+  end
+
+  def authors_own?
+    @user.id == @record.author_id
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user || User.new
+      @scope = scope
+    end
+
+    def resolve
+      if editor?
+        scope.all
+      elsif author?
+        scope.where(author_id: user.id)
+      else
+        scope.where(published: true)
+      end
+    end
+
+    def author?
+      @user.role == "author"
+    end
+
+    def editor?
+      @user.role == "editor"
+    end
+  end
 end
