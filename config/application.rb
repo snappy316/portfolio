4bc5c918174ff601555b535cb82c3af87e12b3bc
David Ker <snappy316@gmail.com>
2015-03-18 11:20:58 -0700

    When generating tests, auto-generate it as a spec

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module Portfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: true, fixture: true
+    end
   end
 end
