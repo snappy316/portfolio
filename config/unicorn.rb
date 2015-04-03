9c0f690a54ec4871fa596d1353a73db7a5106fcd
David Ker <snappy316@gmail.com>
2015-04-03 14:53:59 -0700

    Edit unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,19 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
-worker_processes 3
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
@@ -10,4 +21,11 @@ after_fork do |server, worker|
   Sidekiq.configure_server do |config|
     config.redis = { :size => 5 }
   end
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
