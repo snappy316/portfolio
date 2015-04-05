8deb010c45db40896acfb657bcacdff6bc33393d
David Ker <snappy316@gmail.com>
2015-04-05 15:16:50 -0700

    Generate contact resource, and write out the contact form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331212738) do
+ActiveRecord::Schema.define(version: 20150404175022) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20150331212738) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
