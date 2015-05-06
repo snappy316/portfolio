d7e34994f54e71a53815036f20b8ec53b6a7bab2
David Ker <snappy316@gmail.com>
2015-05-06 15:45:52 -0700

    Generate friendly_id and add slug column to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404175022) do
+ActiveRecord::Schema.define(version: 20150506224012) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(version: 20150404175022) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "technologies_used"
@@ -48,7 +61,10 @@ ActiveRecord::Schema.define(version: 20150404175022) do
     t.datetime "updated_at",        null: false
     t.string   "image"
     t.boolean  "image_processed"
+    t.string   "slug"
   end
+
+  add_index "projects", ["slug"], name: "index_projects_on_slug"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
